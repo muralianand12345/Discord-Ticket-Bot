4938ff26b934f3c404952415608d5d0be597e75c
shadowctrl <shadowctrl@outlook.com>
2022-09-22 22:19:34 +0530

    error log template

---
+++ logs/std_log.js
@@ -0,0 +1,20 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports={
+    async error(client,command="unknown",user="unknown",channel="unknown")
+    {
+        let log_channel=await client.channels.cache.get(client.config.STD_LOG.CHAN_ID);
+        const embed = new EmbedBuilder()
+            .setColor('Green')
+            .setTitle('STD Logger')
+            .addFields(
+                { name:"Command", value:command },
+                { name:"User", value:`<@${user}>`},
+                { name:"channel", value:`<#${channel}>`}
+            );
+
+        await log_channel.send({
+            embeds: [embed]
+        });
+    }
+}
