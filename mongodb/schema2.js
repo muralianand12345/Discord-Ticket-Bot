3ab51606d37f65730006564375fb91a23f4a17dd
Murali Anand <71955467+muralianand12345@users.noreply.github.com>
2022-09-30 23:34:42 +0530

    Delete mongodb directory

--- mongodb/schema2.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model} = require('mongoose');
-
-const details = new Schema({
-    guild_id:Number,
-    global_ticket_no:Number
-})
-
-module.exports = model('Global-Ticket-Number', details);
