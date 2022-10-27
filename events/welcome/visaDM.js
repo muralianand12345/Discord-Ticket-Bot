1f94a19d483c6c7e8d14e8101c86bcbc9e0e46e2
Murali Anand <71955467+muralianand12345@users.noreply.github.com>
2022-10-27 18:25:55 +0530

    🚀 Version 2.6.1
    Added extra 2 channels (optional)

--- events/welcome/visaDM.js
+++ events/welcome/visaDM.js
@@ -9,7 +9,6 @@ module.exports = {
     name: 'messageCreate',
     execute: async (message, client) => {
 
-
         //ACCEPTED
         const VPChan = client.config.DM_MESSAGE.ACCEPTED.VPCHAN;
         if (message.channel.id === VPChan) {
@@ -110,6 +109,117 @@ module.exports = {
                         .setDescription(`DM sent to <@${mentionID}> (VP Response Rej)`)
 
                     client.channels.cache.get(client.config.DM_MESSAGE.REJECTED.LOGCHAN).send({
+                        embeds: [logembed]
+                    });
+                }
+            }
+        }
+
+        //VISAGIVEN
+        const VisaChan = client.config.DM_MESSAGE.VISA.VISACHAN;
+        if (message.channel.id === VisaChan) {
+            if (message.content.includes('<@')) {
+                var mention = message.mentions.users.first();
+                if (mention == null) {
+                    return;
+                } else {
+                    const mentionID = mention.id;
+                    const msgLink = message.url;
+
+                    const embed = new EmbedBuilder()
+                        .setColor('Green')
+                        .setThumbnail(client.config.DM_MESSAGE.LOGO)
+                        .setTitle(`EliteX Voice Process`)
+                        .setDescription(`<@${mentionID}>, Congratulations 🎊! Your **Visa has been approved**, Thanks for attending the Voice Process.\nFor further queries regarding login and connectivity contact staffs in <#${client.config.DM_MESSAGE.VISA.HELPCHAN}>`)
+
+                    const sbutton = new ActionRowBuilder()
+                        .addComponents(
+                            new ButtonBuilder()
+                                .setLabel(`Connect EliteX`)
+                                .setEmoji('🔗')
+                                .setStyle(ButtonStyle.Link)
+                                .setURL(client.config.DM_MESSAGE.CONNECT),
+                            new ButtonBuilder()
+                                .setLabel(`Message`)
+                                .setEmoji('📑')
+                                .setStyle(ButtonStyle.Link)
+                                .setURL(msgLink)
+                        )
+
+                    client.users.cache.get(mentionID).send({
+                        embeds: [embed],
+                        components: [sbutton]
+                    }).catch(error => {
+                        if (error.code == 50007) {
+                            const logembed = new EmbedBuilder()
+                                .setColor('Black')
+                                .setDescription(`Unable to DM <@${mentionID}> (Acc Response)`)
+
+                            return client.channels.cache.get(client.config.DM_MESSAGE.VISA.LOGCHAN).send({
+                                embeds: [logembed]
+                            });
+                        }
+                    })
+
+                    //LOG
+                    const logembed = new EmbedBuilder()
+                        .setColor('Black')
+                        .setDescription(`DM sent to <@${mentionID}> (Acc Response)`)
+
+                    client.channels.cache.get(client.config.DM_MESSAGE.VISA.LOGCHAN).send({
+                        embeds: [logembed]
+                    });
+                }
+            }
+        }
+
+        //ONHOLD
+        const HoldChan = client.config.DM_MESSAGE.HOLD.HOLDCHAN;
+        if (message.channel.id === HoldChan) {
+            if (message.content.includes('<@')) {
+                var mention = message.mentions.users.first();
+                if (mention == null) {
+                    return;
+                } else {
+                    const mentionID = mention.id;
+                    const msgLink = message.url;
+
+                    const embed = new EmbedBuilder()
+                        .setColor('Orange')
+                        .setThumbnail(client.config.DM_MESSAGE.LOGO)
+                        .setTitle(`EliteX Voice Process`)
+                        .setDescription(`<@${mentionID}>, Your visa application is **on Hold** 😔. Kindly read the rules <#${client.config.DM_MESSAGE.HOLD.RULECHAN}> and attend the next voice process.`)
+
+                    const sbutton = new ActionRowBuilder()
+                        .addComponents(
+                            new ButtonBuilder()
+                                .setLabel(`Message`)
+                                .setEmoji('📑')
+                                .setStyle(ButtonStyle.Link)
+                                .setURL(msgLink)
+                        )
+
+                    client.users.cache.get(mentionID).send({
+                        embeds: [embed],
+                        components: [sbutton]
+                    }).catch(error => {
+                        if (error.code == 50007) {
+                            const logembed = new EmbedBuilder()
+                                .setColor('Black')
+                                .setDescription(`Unable to DM <@${mentionID}> (Hold Response)`)
+
+                            return client.channels.cache.get(client.config.DM_MESSAGE.HOLD.LOGCHAN).send({
+                                embeds: [logembed]
+                            });
+                        }
+                    })
+
+                    //LOG
+                    const logembed = new EmbedBuilder()
+                        .setColor('Black')
+                        .setDescription(`DM sent to <@${mentionID}> (Hold Response)`)
+
+                    client.channels.cache.get(client.config.DM_MESSAGE.HOLD.LOGCHAN).send({
                         embeds: [logembed]
                     });
                 }
